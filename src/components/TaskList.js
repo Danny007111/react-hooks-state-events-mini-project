6fd3db920c966a1902a472390e98d9b3d95db8d1
Danny Rodriguez <danny007111@hotmail.com>
2022-06-07 16:24:09 -0500

    working on delete button

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { TASKS } from "../data";
+import Task from "./Task";
+
 
 function TaskList() {
+  const Tasks = TASKS.map((eachTask, index)=>{
+    return <Task key={index} category={eachTask.category} text={eachTask.text}/>
+  })
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {Tasks}
     </div>
   );
 }
